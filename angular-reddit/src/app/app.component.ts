f3c769eb5789133b0ed7aa3cfc54c2f4bddd2f24
Bram Luyten <bram@atmire.com>
2018-10-03 06:55:58 +0200

    ng-book angular-reddit until mid p82

--- angular-reddit/src/app/app.component.ts
+++ angular-reddit/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from "./article/article.model";
 
 @Component({
   selector: 'app-root',
@@ -6,9 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  articles: Article[];
+
+  constructor(){
+    this.articles = [
+      new Article('DSpace 7 rocks', 'https://www.dspace.org', 7),
+      new Article('Migrating away from EPrints', 'https://www.eprints.org', 4),
+      new Article('ORCID feature for Open Repository', 'https://www.openrepository.com', 3)
+    ]
+  }
 
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 3));
+    title.value ='';
+    link.value = '';
     return false;
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
   }
 }
